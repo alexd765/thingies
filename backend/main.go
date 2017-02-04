3170ae2375276a7ebcae4eb90bf14c4e44f2758a
Alexander Döring <email@alexd.ch>
2017-02-04 21:39:55 +0100

    backend: access the image on s3 and report its size

--- backend/main.go
+++ backend/main.go
@@ -6,7 +6,12 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/eawsy/aws-lambda-go-core/service/lambda/runtime"
 )
 
@@ -20,6 +25,19 @@ type Task struct {
 type Result struct {
 	Version     int    `json:"version"`
 	OutputToken string `json:"output-token"`
+	Size        int    `json:"size"`
+}
+
+var s3Client *s3.S3
+
+func init() {
+	creds := credentials.NewEnvCredentials()
+	_, err := creds.Get()
+	if err != nil {
+		log.Fatalf("failed to get creds: %s", err)
+	}
+	sess := session.New(aws.NewConfig().WithRegion("eu-west-1").WithCredentials(creds))
+	s3Client = s3.New(sess)
 }
 
 // Handle is our lambda function.
@@ -32,9 +50,18 @@ func Handle(evt json.RawMessage, ctx *runtime.Context) (interface{}, error) {
 		return nil, err
 	}
 
+	obj, err := s3Client.GetObject(&s3.GetObjectInput{Bucket: aws.String("thingies-input"), Key: aws.String(task.InputToken)})
+	if err != nil {
+		return nil, err
+	}
+	var size int
+	if obj.ContentLength != nil {
+		size = int(*obj.ContentLength)
+	}
 	result := Result{
-		Version:     1,
+		Version:     2,
 		OutputToken: task.InputToken,
+		Size:        size,
 	}
 	return result, nil
 }
